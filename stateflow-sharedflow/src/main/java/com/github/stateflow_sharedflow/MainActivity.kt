e3b761e0c08dfac30f8e1013e987f720f3832210
K januprasad <jnuprasad@gmail.com>
2024-06-27 15:11:50 +0530

    proper state handling

--- stateflow-sharedflow/src/main/java/com/github/stateflow_sharedflow/MainActivity.kt
+++ stateflow-sharedflow/src/main/java/com/github/stateflow_sharedflow/MainActivity.kt
@@ -6,19 +6,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -36,7 +46,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             InterviewprepTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyApp()
+                    MyApp(paddingValues = innerPadding)
                 }
             }
         }
@@ -44,30 +54,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(viewModel: MainViewModel = viewModel()) {
+fun MyApp(paddingValues: PaddingValues, viewModel: MainViewModel = viewModel()) {
     var state by remember {
         mutableStateOf("")
     }
-    LaunchedEffect(key1 = true) {
-        viewModel.testChannel.receiveAsFlow().collect {
-            state = when (it) {
-                UIEvent.Green -> {
-                    it.toString()
-                }
-
-                UIEvent.Red -> {
-                    it.toString()
-                }
-            }
-        }
+    var uiState = viewModel.testState.collectAsState()
+    var name by remember {
+        mutableStateOf("")
     }
-    Box(modifier = Modifier
+
+    var text by remember { mutableStateOf(uiState.value.name) }
+    var age by remember { mutableStateOf("") }
+//    LaunchedEffect(key1 = true) {
+//        viewModel.testChannel.receiveAsFlow().collect {
+//            state = when (it) {
+//                UIEvent.Green -> {
+//                    it.toString()
+//                }
+//
+//                UIEvent.Red -> {
+//                    it.toString()
+//                }
+//            }
+//        }
+//    }
+    Column(modifier = Modifier
         .fillMaxSize()
+        .padding(paddingValues)
         .drawBehind {
             drawRect(
                 color = resolveState(state)
             )
-        })
+        }
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = name,
+            onValueChange = {
+                name = it
+            },
+            label = { Text("Name") }
+        )
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = age.toString(),
+            onValueChange = {
+                age = it
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+            label = { Text("Age") }
+        )
+        Button(onClick = { viewModel.updateState(name, age) }) {
+            Text(text = "Submit")
+        }
+    }
 }
 
 fun resolveState(state: String): Color {
--- stateflow-sharedflow/src/main/java/com/github/stateflow_sharedflow/MainViewModel.kt
+++ stateflow-sharedflow/src/main/java/com/github/stateflow_sharedflow/MainViewModel.kt
@@ -1,16 +1,28 @@
 package com.github.stateflow_sharedflow
 
+import android.util.Log
+import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
+data class UIState(
+    var name: String = "",
+    var id: Int = 0
+)
 
 class MainViewModel : ViewModel() {
     val testEvent = MutableSharedFlow<UIEvent>()
+    private val _testState = MutableStateFlow(UIState())
+    var testState = _testState.asStateFlow()
     val testChannel = Channel<UIEvent>()
 
     init {
@@ -28,6 +40,7 @@ class MainViewModel : ViewModel() {
             testEvent.emit(UIEvent.Red)
         }
     }
+
     private fun startChannel() {
         viewModelScope.launch {
             delay(1000)
@@ -36,6 +49,14 @@ class MainViewModel : ViewModel() {
             testChannel.send(UIEvent.Green)
             delay(5000)
             testChannel.send(UIEvent.Red)
+        }
+    }
+
+    fun updateState(name: String, id: String) {
+        viewModelScope.launch(Dispatchers.Default) {
+            _testState.update { state ->
+                state.copy(name = name, id = id.toInt())
+            }
         }
     }
 }
