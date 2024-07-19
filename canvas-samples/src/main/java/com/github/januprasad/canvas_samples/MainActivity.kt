58c561063c0515df5be4e7e862770363d2fa2114
K januprasad <jnuprasad@gmail.com>
2024-07-19 11:32:02 +0530

    bazeir curve test

--- canvas-samples/src/main/java/com/github/januprasad/canvas_samples/MainActivity.kt
+++ canvas-samples/src/main/java/com/github/januprasad/canvas_samples/MainActivity.kt
@@ -54,12 +54,18 @@ fun SampleApp(innerPadding: PaddingValues) {
     Column(Modifier.padding(innerPadding)) {
         getWeatherApp()
         instagramIcon()
+        face()
+        bazeiercurves()
     }
 }
 
 @Composable
-fun getWeatherApp() {
-    val backgroundColor = listOf(Color(0xFF2078EE), Color(0xFF74E6FE))
+fun bazeiercurves() {
+}
+
+@Composable
+fun face() {
+    val backgroundColor = listOf(Color(0xFF2454ff), Color(0xFF423234))
     val sunColor = listOf(Color(0xFFFFC200), Color(0xFFFFE100))
     Canvas(
         modifier =
@@ -113,12 +119,66 @@ fun getWeatherApp() {
             center = Offset(width.times(.75f), height.times(.35f)),
         )
         drawRoundRect(
-            topLeft = Offset(size.width / 3, 140f),
-            size = Size(size.width / 3, 10f),
+            topLeft = Offset(width.times(0.33f), height.times(0.7f)),
+            size = Size(width.times(0.33f), height.times(0.05f)),
             brush = Brush.verticalGradient(sunColor),
             cornerRadius = CornerRadius(20f, 20f),
         )
 //        drawPath(path = path, color = Color.White.copy(alpha = .90f))
+    }
+}
+
+@Composable
+fun getWeatherApp() {
+    val backgroundColor = listOf(Color(0xFF2078EE), Color(0xFF74E6FE))
+    val sunColor = listOf(Color(0xFFFFC200), Color(0xFFFFE100))
+    Canvas(
+        modifier =
+            Modifier
+                .size(100.dp)
+                .padding(16.dp),
+    ) {
+        val width = size.width
+        val height = size.height
+        val path =
+            Path().apply {
+                moveTo(width.times(.76f), height.times(.72f))
+                cubicTo(
+                    width.times(.93f),
+                    height.times(.72f),
+                    width.times(.98f),
+                    height.times(.41f),
+                    width.times(.76f),
+                    height.times(.40f),
+                )
+                cubicTo(
+                    width.times(.75f),
+                    height.times(.21f),
+                    width.times(.35f),
+                    height.times(.21f),
+                    width.times(.38f),
+                    height.times(.50f),
+                )
+                cubicTo(
+                    width.times(.25f),
+                    height.times(.50f),
+                    width.times(.20f),
+                    height.times(.69f),
+                    width.times(.41f),
+                    height.times(.72f),
+                )
+                close()
+            }
+        drawRoundRect(
+            brush = Brush.verticalGradient(backgroundColor),
+            cornerRadius = CornerRadius(50f, 50f),
+        )
+        drawCircle(
+            brush = Brush.verticalGradient(sunColor),
+            radius = width.times(.17f),
+            center = Offset(width.times(.35f), height.times(.35f)),
+        )
+        drawPath(path = path, color = Color.White.copy(alpha = .90f))
     }
 }
 
