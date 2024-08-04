7ffd93aa0c69b96682cb18e58bde40cbef2565b2
Januprasad <jnuprasad@gmail.com>
2024-08-04 15:03:55 +0530

    state diff

--- sample-xml-app/src/main/java/com/example/sample_xml_app/MainActivity.kt
+++ sample-xml-app/src/main/java/com/example/sample_xml_app/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.sample_xml_app
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MyViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,5 +31,36 @@ class MainActivity : AppCompatActivity() {
         score_bt.setOnClickListener {
             viewModel.scorePoint()
         }
+        Log.v("Application", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v("Application", "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v("Application", "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v("Application", "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v("Application", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v("Application", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v("Application", "onDestroy")
     }
 }
--- sample-xml-app/src/main/java/com/example/sample_xml_app/MyViewModel.kt
+++ sample-xml-app/src/main/java/com/example/sample_xml_app/MyViewModel.kt
@@ -1,26 +1,33 @@
 package com.example.sample_xml_app
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
-
-    val game = Game() //init the object
+    val game = Game() // init the object
     val _userScore = MutableLiveData<Game>()
     val userScore: LiveData<Game>
         get() = _userScore
 
     init {
-        _userScore.value = game.apply {
-            score = 1
-        }
+        Log.v("Application", "VM init")
+        _userScore.value =
+            game.apply {
+                score = 1
+            }
     }
 
     fun scorePoint() {
-        _userScore.value = game.apply {
-            score++
-        }
+        _userScore.value =
+            game.apply {
+                score++
+            }
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        Log.v("Application", "VM onCleared")
+    }
 }
