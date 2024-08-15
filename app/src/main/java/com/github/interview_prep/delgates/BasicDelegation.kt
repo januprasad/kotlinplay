dbb22453c76358d2374109296bc8c1a5e4ecadfc
Januprasad <jnuprasad@gmail.com>
2024-08-15 12:41:16 +0530

    delegations

--- app/src/main/java/com/github/interview_prep/delgates/BasicDelegation.kt
+++ app/src/main/java/com/github/interview_prep/delgates/BasicDelegation.kt
@@ -1,12 +1,32 @@
 package com.github.interview_prep.delgates
 
 /**
- * In this example, the Derived class delegates the print()
- * method to the BaseImpl instance, reusing its implementation without code duplication.
+ * In this example, the VendorMachine,SnackMachine  class delegates the getSnacks()
+ * method to the AgentVendorMachine instance, reusing its implementation without code duplication.
  */
 interface Base {
     fun print()
 }
+
+interface Machine {
+    fun getSnacks(): String
+}
+
+class VendorMachine : Machine {
+    val snacks = listOf("Kitkat", "Cadburry", "Apple Juice", "Moosambi")
+
+    override fun getSnacks(): String = snacks.random()
+}
+
+class SnackMachine : Machine {
+    val snacks = listOf("Kitkat", "Cadburry")
+
+    override fun getSnacks(): String = snacks.random()
+}
+
+class AgentVendorMachine(
+    vendorMachine: Machine,
+) : Machine by vendorMachine
 
 class BaseImpl(
     val x: Int,
@@ -18,10 +38,17 @@ class BaseImpl(
 
 class Derived(
     b: Base,
-) : Base by b // Delegation
+) : Base by b
+
+class Derived1(
+    b: Base,
+)
 
 fun main() {
-    val b = BaseImpl(10)
-    val d = Derived(b)
-    d.print() // Output: BaseImpl 10
+    val machine1 = VendorMachine()
+    val machine2 = SnackMachine()
+    val vendorMachine1 = AgentVendorMachine(machine1)
+    val vendorMachine2 = AgentVendorMachine(machine2)
+    println(vendorMachine1.getSnacks())
+    println(vendorMachine2.getSnacks())
 }
