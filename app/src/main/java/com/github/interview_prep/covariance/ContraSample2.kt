f977f85d957599958e1a41895d9d2e1cd94987ae
Januprasad <jnuprasad@gmail.com>
2024-09-11 18:18:26 +0530

    contra

--- app/src/main/java/com/github/interview_prep/covariance/ContraSample2.kt
+++ app/src/main/java/com/github/interview_prep/covariance/ContraSample2.kt
@@ -1,6 +1,6 @@
 package com.github.interview_prep.covariance
 
-interface Consumer<in T> {
+interface IConsumer<in T> {
     fun consume(t: T)
 }
 
@@ -10,22 +10,22 @@ interface Consumer<in T> {
  * List<in T> in Kotlin is equivalent to List<? super T> in Java
  */
 
-class AnimalConsumer : Consumer<Animal> {
+class AnimalConsumer : IConsumer<Animal> {
     override fun consume(animal: Animal) {
         println("Consuming animal: $animal")
     }
 }
 
 // This works because Animal is a supertype of Cat
-fun feed(consumer: Consumer<Cat>) {
+fun feed(consumer: IConsumer<Cat>) {
     consumer.consume(Cat())
 }
 
 fun main() {
     // This works because Animal is a supertype of Cat
-    val animalConsumer: Consumer<Animal> = AnimalConsumer()
+    val animalConsumer: IConsumer<Animal> = AnimalConsumer()
     feed(animalConsumer) // open this feed you will know
 
-    val catConsumer: Consumer<Cat> = AnimalConsumer()
+    val catConsumer: IConsumer<Cat> = AnimalConsumer()
     feed(catConsumer)
 }
