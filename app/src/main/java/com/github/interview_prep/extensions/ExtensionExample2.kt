66401dcd7aa96ce3f586e379c8a20740aea5f8e8
Januprasad <jnuprasad@gmail.com>
2024-10-02 09:07:33 +0530

    extension exmple update

--- app/src/main/java/com/github/interview_prep/extensions/ExtensionExample2.kt
+++ app/src/main/java/com/github/interview_prep/extensions/ExtensionExample2.kt
@@ -1,9 +1,16 @@
 package com.github.interview_prep.extensions
 
 fun main() {
-    println(3 (2 + 2))
-    println(3 + (2 + 2))
-    println(3 + (2))
+    println(11(11 + 11(22 + 22(33 + 33))))
+    println(11(11 + 11(22 + 22(66))))
+    println(11(11 + 11(22 + 1452)))
+    println(11(11 + 11(1474)))
+    println(11(11 + 16214))
+    println(11(16225))
+    println(178475)
+    println(11(11 + 22(33 + 44)))
+    println(11(11 + 22(33 + 55)))
+    println(12 * 12 (12))
 }
 
-private operator fun Int.invoke(value: Int) = this + value
+operator fun Int.invoke(value: Int) = this * value
---
+++ app/src/main/java/com/github/interview_prep/extensions/Sample.kt
@@ -0,0 +1,9 @@
+package com.github.interview_prep.extensions
+
+fun main() {
+    val dividend = 7
+//    val divisor = 0
+    repeat(dividend) { divisor ->
+        println("$dividend - $divisor")
+    }
+}
